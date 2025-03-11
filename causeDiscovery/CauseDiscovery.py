cb63f3fc24b8eaddca2d97f9d446659106ff8e37
jkrstww <jk2258304041@163.com>
2025-03-11 18:09:16 +0800

    commit reference

--- causeDiscovery/CauseDiscovery.py
+++ causeDiscovery/CauseDiscovery.py
@@ -1,17 +1,20 @@
+from abc import abstractmethod
+
 import openai
 from normalUtils import *
 
 class CauseDiscovery():
-    variables_path = ''
-    write_path = ''
+    file_path = ''
+    output_path = './output.txt'
     variables = set()
 
-    def __init__(self):
-        self.write_path = './output.txt'
+    def __init__(self, file_path, output_path):
+        self.file_path = file_path
+        self.output_path = output_path
 
-    def get_variables(self, variables_path):
-        encoding = get_encoding(variables_path)
-        with open(variables_path, 'r', encoding=encoding) as f:
+    def get_variables(self, file_path):
+        encoding = get_encoding(file_path)
+        with open(file_path, 'r', encoding=encoding) as f:
             lines = f.readlines()
         f.close()
 
@@ -22,6 +25,9 @@ class CauseDiscovery():
     def show_variables(self):
         print(self.variables)
 
+    @abstractmethod
+    def run(self):
+        pass
 
 if __name__ == '__main__':
     cd = CauseDiscovery()
